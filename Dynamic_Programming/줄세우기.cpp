d65018b64d16df7e60d126870954c479b96f21d9
taehun2767 <73827343+taehun2767@users.noreply.github.com>
2023-09-02 23:14:18 -0700

    줄세우기 using DP

---
+++ Dynamic_Programming/줄세우기.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+
+#define MAX 200
+
+using namespace std;
+
+
+int n;
+int childrens[MAX];
+vector<int> cache[MAX];
+
+void insert(vector<int>& vec, int num){
+    if(vec[0] >= num)
+        return ;
+
+    if(vec[vec.size()-1] < num)
+    {
+        vec.push_back(num);
+        return ;
+    }
+
+    int i = vec.size() - 1;
+    while(i > 0) {
+        if (vec[i] > num && vec[i - 1] < num) {
+            vec[i] = num;
+            return;
+        }
+
+        if (vec[i] > num && vec[i - 1] > num)
+            i--;
+        else {
+            break;
+        }
+    }
+}
+
+void solution(){
+    int maxCount = 0;
+    for(int i = 0; i < n; i++)
+    {
+        cache[i].push_back(childrens[i]);
+        for(int j = i + 1; j < n; j++)
+        {
+            insert(cache[i], childrens[j]);
+        }
+
+        maxCount = max(maxCount, (int)cache[i].size());
+    }
+    cout << n - maxCount;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> childrens[i];
+    }
+
+    solution();
+    
+}
