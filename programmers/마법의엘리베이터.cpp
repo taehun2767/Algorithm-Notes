fd02ecfac6dcef3603fd84ac0264c571a5e11fb8
taehun2767 <73827343+taehun2767@users.noreply.github.com>
2023-10-21 00:53:36 +0900

    using backtracking

---
+++ programmers/마법의엘리베이터.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int backtracking(int storey, int count){
+    if(storey < 10){
+        return min(storey + count, 10 - storey + 1 + count);
+    }
+    
+    int ret = 10000000;
+    ret = min(ret, backtracking(storey / 10, count + storey % 10));
+    ret = min (ret, backtracking(storey / 10  + 1, count + 10 - storey % 10));
+    
+    return ret;
+}
+
+int solution(int storey) {
+    int answer = 0;
+    answer = backtracking(storey, 0);
+    return answer;
+}
