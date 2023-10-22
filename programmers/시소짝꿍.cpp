4a265a9a582b06949933884be841aeb147e80276
taehun2767 <73827343+taehun2767@users.noreply.github.com>
2023-10-22 22:50:42 +0900

    using dp

---
+++ programmers/시소짝꿍.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+#define MAX 2001
+using namespace std;
+long long dp[2001];
+bool visit[2001];
+long long solution(vector<int> weights) {
+    long long answer = 0;
+    for(auto i : weights)
+        dp[i] += 1;
+    for(auto i : weights){
+        if(dp[i] > 1 && visit[i] == false){
+            answer +=  dp[i] * (dp[i] - 1) / 2;
+            visit[i] = true;
+        }
+        if(2 * i < MAX && dp[2 * i])
+            answer += dp[2 * i];
+        if(i % 3 == 0 && i/3 * 4 < MAX && dp[i/3 * 4])
+            answer += dp[i/3 * 4];
+        if(i % 2 == 0 && i/2 * 3 < MAX && dp[i/2 * 3])
+            answer += dp[i/2 * 3];
+    } 
+    return answer;
+}
