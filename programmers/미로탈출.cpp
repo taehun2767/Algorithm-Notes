71b1422dedf00199ed673db1bc053e12bf054dec
taehun2767 <73827343+taehun2767@users.noreply.github.com>
2023-11-04 00:42:21 +0900

    using bfs

--- programmers/미로탈출.cpp
+++ programmers/미로탈출.cpp
@@ -1,10 +1,57 @@
 #include <string>
 #include <vector>
+#include <queue>
 #include <iostream>
 
 using namespace std;
 
-// int bfs()
+int dr[4] = {0, 1, 0, -1};
+int dc[4] = {1, 0, -1, 0};
+
+struct posInfo{
+    int r;
+    int c;
+    int distance;
+};
+
+int bfs(vector<string> maps, vector<int> source, vector<int> destination){
+    
+    vector<vector<bool>> visited = vector<vector<bool>>(maps.size(), vector<bool>(maps[0].size(), false));
+    queue<posInfo> que;
+    
+    posInfo cur;
+    cur.r = source[0];
+    cur.c = source[1];
+    cur.distance = 0;
+    que.push(cur);
+    visited[cur.r][cur.c] = true;
+    
+    while(que.empty() == false){
+        cur = que.front();
+        que.pop();
+        if(cur.r == destination[0] && cur.c == destination[1])
+            return cur.distance;
+        
+        for(int i = 0; i < 4; i++){
+            int nr = cur.r + dr[i];
+            int nc = cur.c + dc[i];
+            
+            if(nr < 0 || nr >= maps.size() || nc < 0 || nc >= maps[0].size())
+                continue;
+            if(visited[nr][nc] || maps[nr][nc] == 'X')
+                continue;
+            
+            posInfo next;
+            next.r = nr;
+            next.c = nc;
+            next.distance = cur.distance + 1;
+            que.push(next);
+            visited[nr][nc] = true;
+        }
+    }
+    
+    return -1;
+}
 
 int solution(vector<string> maps) {
     int answer = 0;
@@ -25,6 +72,14 @@ int solution(vector<string> maps) {
             }
         }
     }
-
-    return answer;
+    
+    int startToLever = bfs(maps, pos[0], pos[1]);
+    if(startToLever == -1)
+        return -1;
+    int leverToEnd = bfs(maps, pos[1], pos[2]);
+    if(leverToEnd == -1)
+        return -1;
+    
+    
+    return startToLever + leverToEnd;
 }
