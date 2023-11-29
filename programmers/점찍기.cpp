30d6693ef7aa1ebf346bd2bd9113bff828ddc21e
taehun2767 <73827343+taehun2767@users.noreply.github.com>
2023-11-30 00:32:16 +0900

    using math

--- programmers/점찍기.cpp
+++ programmers/점찍기.cpp
@@ -1,7 +1,12 @@
-long long solution(int k, int d)
-{
-    long long answer = d/k + 1;
-    for(long long i=0;i<d;i+=k)
-        answer += (long long)sqrt(1LL*d*d - i*i)/k;
+#include <cmath>
+
+using namespace std;
+
+long long solution(int k, int d) {
+    long long answer = 0;
+    for(long i = 0; i <= d; i += k){
+        answer += (long) sqrt(1L*d*d - i * i) / k + 1;
+    }
+    
     return answer;
 }
