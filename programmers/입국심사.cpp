457ce9e4a0b25c268a501fb59c211c4e33866b0e
taehun2767 <73827343+taehun2767@users.noreply.github.com>
2023-12-03 01:37:53 +0900

    using 이분탐색

--- programmers/입국심사.cpp
+++ programmers/입국심사.cpp
@@ -6,46 +6,34 @@
 
 using namespace std;
 
-struct info{
-    long long time;
-    long long lastTime;
-};
-
-struct  comp{
-    bool operator()(info a, info b){
-        return  a.lastTime + a.time >  b.lastTime + b.time;
-    }
-};
-
 
 long long solution(int n, vector<int> times) {
     long long answer = 0;
+    sort(times.begin(), times.end());
+    
+    long long min = 1;
+    long long max = n * (long long)times.back();
 
-    if(n < times.size()){
-        sort(times.begin(), times.end());
-        return times[n-1];
+    
+
+    while(min <= max){
+        long long mid = (min + max ) / 2 ;
+        
+        long long avg = 0;
+    
+        for(auto t : times){
+            avg += mid / (long long)t;
+        }
+        
+        if (avg >= n){
+            max = mid - 1;
+            answer = mid;
+        }
+        else
+            min = mid + 1;
+
     }
+    
+    return answer;
 
-    priority_queue<info, vector<info>, comp> pq;
-
-    for(auto t : times){
-        info cur;
-        cur.lastTime = t;
-        cur.time = t;
-        pq.push(cur);
-        n--;
-    }
-    while(n > 0){
-        info cur = pq.top();
-        pq.pop();
-        cur.lastTime += cur.time;
-        pq.push(cur);
-        n--;
-        if(n ==0)
-            return cur.lastTime;
-    }
-
-
-
-    return -1;
 }
